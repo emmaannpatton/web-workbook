86644ff77d4f952ec8335bd81cdb4bbe72457205
Emma Ann Patton <emma.patton@utexas.edu>
2018-08-29 17:28:42 -0500

    calc added

--- 07week/calculator/js/script.js
+++ 07week/calculator/js/script.js
@@ -2,4 +2,48 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
   // You code here
+  function addNumber(num) {
+  document.querySelector("#results").value += num;
+}
+
+function clearResults() {
+  document.querySelector("#results").value = "";
+}
+
+function addition() {
+  document.querySelector("#results").value += "+";
+}
+
+function subtraction() {
+  document.querySelector("#results").value += "-";
+}
+
+function multiplication() {
+  document.querySelector("#results").value += "*";
+}
+
+function division() {
+  document.querySelector("#results").value += "/";
+}
+
+function makeNegative(addNumber) {
+ document.querySelector("#results").value += "*-1";
+  equals();
+}
+
+function equals() {
+  document.querySelector("#results").value = eval(document.querySelector("#results").value);
+}
+
+// function removeZero (str) {
+//   var good= ''
+//   for(let i = 0; i < str.length; i++) {
+//   if(str[i])
+//   }
+// }
+
+function deleteLast() {
+  let current = document.querySelector("#results").value;
+  document.querySelector("#results").value = current.slice(0, -1);
+}
 });
