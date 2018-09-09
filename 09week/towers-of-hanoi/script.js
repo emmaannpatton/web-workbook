b16611b0ca82681f2ed6db9ddbb305766e3b1849
Emma Ann Patton <emma.patton@utexas.edu>
2018-09-08 19:33:30 -0500

    saving

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,52 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
+
+
 });
+
+$(function(){
+ $('.draggable').draggable({
+   revert: 'invalid'
+ });
+ $('.dropArea').droppable({
+   drop: function(event, ui){
+     // check if ok
+     let $last =
+     ($(this).children().last()).data('block');
+     //console.log('last element on stack');
+     let $dragging = $(ui.draggable).data('block');
+     //console.log('dragging element ${$dragging}');
+     if($dragging > $last{
+        $(ui.draggable).draggable('option' 'revert', true);
+      }else{
+         $(ui.draggable).appendTo(this).attr('style', 'position: "relative"');)
+         checkWin();
+   //
+   //   })
+   //   $(ui.draggable).appendTo(this).attr('style', 'position: "relative"');
+   //   console.log("this is being dragged here")
+   // }
+ });
+ function checkWin(){
+   if ($('[data-stack=3], [data-stack=2]')
+ }
+})
+
+
+
+
+//writing a function for if else {
+
+// $(function(){
+//   if('[data-block]' > .this)
+//   $(ui.draggable).appendTo(this).attr('style', 'position: "relative"');
+// }else {
+//   $('.draggable').draggable({
+//     revert: 'invalid'
+// }
+// }
+
+// if (condition) {
+//     block of code to be executed if the condition is true
+// }
